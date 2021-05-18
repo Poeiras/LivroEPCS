d7ee01513790903c2464dc6a6b9558234c3dfec7
Lucas Poeiras <poeiras666@gmail.com>
2021-05-18 01:44:35 -0300

    update

--- Capitulo1.py
+++ Capitulo1.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 from scipy import stats
+
+# Importação de dados e tratamento
 dados = pd.read_csv('data/state.csv')
 colunas = [
     'Estado',
@@ -9,12 +12,13 @@ colunas = [
 ]
 dados.columns = colunas
 
-# Exercício 1
+# Exercícios
 media = dados['População'].mean()
 mediana = dados['População'].median()
 media_aparada = stats.trim_mean(dados['População'], 0.1)
 desvio_padrao = dados['População'].std()
 amplitude_interquartil = stats.iqr(dados['População'])
+percentis = np.quantile(dados['Taxa'], [.5, .25, .5, .75, .95])
 
 # Respostas
 print('Repostas:')
@@ -22,3 +26,4 @@ print('Média: ' + '%0.0f' % media)
 print('Media aparada: ' + '%0.0f' % media_aparada)
 print('Mediana: ' + '%0.0f' % mediana)
 print('Amplitude interquartil: ' + '%0.0f' % amplitude_interquartil)
+print('Percentis de taxa de homicídio: ' + '\n' + '\n'.join('{} : {}'.format(*k) for k in enumerate(percentis)))
